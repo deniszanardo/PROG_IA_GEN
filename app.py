5f570651eb1f9f5f8c7c0e4080195f738d245b29
Denis Zanardo <deniszanardo@gmail.com>
2026-06-27 16:56:13 -0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,104 @@
+# procoding 
+# vou  usar a biblioteca
+import streamlit as st
+import pandas as pd
+
+
+# st.title('CALCULADORA...')
+
+
+# n1  = st.number_input('numero:', )
+# n2  = st.number_input('numero:', value = 0.0)
+
+
+
+# if st.button('Soma'):
+#     calcular   =  n1  + n2 
+#     st.success(calcular)
+# elif st.button('subtração'):    
+#     calcular   =  n1  - n2 
+#     st.success(calcular)    
+# elif st.button('Divisão'):    
+#     calcular   =  n1  / n2 
+#     st.success(calcular)       
+# elif st.button('Multiplicação'):    
+#     calcular   =  n1  * n2 
+#     st.success(calcular)      
+
+
+#DESAFIO 1 
+# st.title('Seja bem vindo ao meu cartão de visita')
+
+# st.header('Denis Renan José Ferreira')
+
+# st.text('37 anos, Comprador, Estudando Python')
+
+# st.markdown("teste 1 ")
+#streamlit run app.py
+#DESAFIO 2
+
+st.image('')
+st.title('Sabiá Alimentos')
+st.header('Ambiente dedicado aos clientes')
+
+st.markdown("Acesse suas Notas Fiscais, Boletos")
+
+
+st.text_input('Digite seu CNPJ: ')
+
+st.text_input('Digite Senha: ')
+st.button('Acesse ')
+
+
+st.button('Esqueci Senha')
+
+
+            
+st.title("Pesquisa rápida")
+
+st.selectbox('select', ['Nota Fiscal','Boleto', 'Pedido'])
+
+st.text_input('')
+                                                                                        
+st.button('Pesquisar')
+
+#st.checkbox('Recupere a Senha: ')
+
+# #st.table()
+# notas_fiscais = pd.DataFrame( 
+#     {
+#         "Nota fiscal": [55, 65, 29],
+#         "nota Fiscal": [23, 63, 77],
+#     }
+
+# )
+# st.dataframe()
+
+
+
+st.markdown("Sabiá Alimentos")
+
+st.markdown("Produzindo gerações desde 1973")
+
+
+st.markdown("Acesse nosso site")
+st.markdown("www.racoessabia.com.br")
+
+#DESAFIO 3
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#DESAFIO 4
