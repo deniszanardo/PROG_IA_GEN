7274e590369b5cc85628f5b690c2f790d0434733
Denis Zanardo <deniszanardo@gmail.com>
2026-07-04 17:41:10 -0300

    Add files via upload

---
+++ AULA 3 - Aprendizado de Máquina/atividade_promps.py
@@ -0,0 +1,8 @@
+Uma escola deseja divulgar cursos de tecnologia.
+
+Contexto: Uma empresa quer migrar para a nuvem. 
+Objetivo: Explicar benefícios da computação em nuvem. 
+Estilo: Educativo. Tom: Profissional. Audiência: 
+Gestores de TI. 
+Formato da resposta: 5 tópicos explicados.
+
